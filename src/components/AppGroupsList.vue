f14cd204276560c3e7b15c1f5b0ef6f68cbdcbb4
slava34 <ch34slava@gmail.com>
2023-02-06 16:37:20 +0300

    fix

--- src/components/AppGroupsList.vue
+++ src/components/AppGroupsList.vue
@@ -3,7 +3,8 @@
     <div class="accordion accordion-flush" v-for="group in groups" :key="group.id" :id="`group-${group.id}`">
       <div class="accordion-item">
         <h2 class="accordion-header">
-          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" :data-bs-target="`#body-${group.id}`">
+          <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                  :data-bs-target="`#body-${group.id}`">
             {{ group.name }}
           </button>
         </h2>
@@ -17,7 +18,13 @@
                   v-for="item in group.items" :key="item.id"
                   class="list-group-item"
               >
-                <app-modal-window :item="item"></app-modal-window>
+                <button
+                    type="button" class="btn btn-primary" data-bs-toggle="modal"
+                    data-bs-target="#item-modal"
+                    @click="UpdateItems(item)"
+                >
+                  {{ item.name }}
+                </button>
               </li>
             </ul>
             <h3 v-else>Нет Подгруппы</h3>
@@ -25,15 +32,27 @@
         </div>
       </div>
     </div>
+    <app-modal-window :item="item"></app-modal-window>
   </div>
 </template>
 
 <script>
 import AppModalWindow from "@/components/AppModalWindow";
+
 export default {
-name: "AppGroupsList",
+  name: "AppGroupsList",
+  data() {
+    return {
+      item: [],
+    }
+  },
   components: {AppModalWindow},
-  props: ['groups', 'items']
+  props: ['groups', 'items'],
+  methods: {
+    UpdateItems(el) {
+      this.item = el;
+    }
+  }
 }
 </script>
 
--- src/components/AppModalWindow.vue
+++ src/components/AppModalWindow.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <!-- Кнопка-триггер модального окна -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" :data-bs-target="`#item-${item.id}`">
-      {{ item.name }}
-    </button>
-
-    <!-- Модальное окно -->
-    <div class="modal fade" :id="`item-${item.id}`" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="item-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
